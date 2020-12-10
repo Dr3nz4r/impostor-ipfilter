227a7c58eceaf358ddcc6b64c7a4a563e90145c3
Administrator Administrator <admin@spjhagenbeckdev.onmicrosoft.com>
2020-12-10 20:52:18 +0100

    Make BlockedMessage configurable

--- impostor-ipfilter/ipfilter/GameEventListener.cs
+++ impostor-ipfilter/ipfilter/GameEventListener.cs
@@ -29,7 +29,7 @@ namespace IpFilter
                 var clientIp = e.Game.Host.Client.Connection.EndPoint.Address.ToString();
                 if (IsClientBlockedFromCreatingLobbies(clientIp)) {
                     _logger.LogInformation($"Player {e.Game.Host.Character.PlayerInfo.PlayerName} with IP: {e.Game.Host.Client.Connection.EndPoint.Address} triedto create a game lobby");
-                    await e.Game.Host.Client.DisconnectAsync(DisconnectReason.Custom, "You are not allowed to create lobbies.");
+                    await e.Game.Host.Client.DisconnectAsync(DisconnectReason.Custom, IpFilterSettings.BlockedMessage);
                 }
             });
         }
--- impostor-ipfilter/ipfilter/IpFilterSettings.cs
+++ impostor-ipfilter/ipfilter/IpFilterSettings.cs
@@ -8,10 +8,13 @@ namespace IpFilter
         public bool BlockListEnabled { get; set; }
         public List<string> Allowed { get; set; }
         public List<string> Blocked { get; set; }
-        
+        public string BlockedMessage { get; set; }
+
+
         public IpFilterSettings() {
             Allowed = new List<string>();
             Blocked = new List<string>();
+            BlockedMessage = "You are not allowed to create lobbies.";
         }
     }
 }
